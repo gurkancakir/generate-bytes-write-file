1d0635a0d64b0e27fccb30198d76e9fb2de025cb
Gurkan Mustafa Cakir <gurkanmustafa.cakir@dxc.com>
2023-01-30 16:44:24 +0100

    writeToFile method updated

--- src/main/java/com/luxoft/FileWriter.java
+++ src/main/java/com/luxoft/FileWriter.java
@@ -1,19 +1,41 @@
 package com.luxoft;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class FileWriter {
 
     public void writeToFile(byte[] contentInBytes, File file) throws IOException {
         long startTime = System.nanoTime();
-        try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
-            fileOutputStream.write(contentInBytes);
-            fileOutputStream.flush();
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw"); FileChannel fileChannel = randomAccessFile.getChannel()) {
+
+            int bufferSize = 1000;
+            ByteBuffer buff = ByteBuffer.allocate(bufferSize);
+
+            int chunkSize = 1000;
+            int limit = (int) Math.ceil((double) contentInBytes.length / chunkSize);
+
+            AtomicInteger iterate = new AtomicInteger();
+            IntStream.iterate(0, i -> i + chunkSize)
+                    .limit(limit)
+                    .mapToObj(index -> Arrays.copyOfRange(contentInBytes, index, Math.min(contentInBytes.length, index + chunkSize)))
+                    .forEach(bytes -> {
+                        try {
+                            fileChannel.write(buff.put(bytes), iterate.getAndIncrement() * chunkSize);
+                            buff.clear();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
         }
         long stopTime = System.nanoTime();
-        System.out.printf("Write Time V1 : %2.2f ms \n", nanoTimeToMillis(startTime, stopTime));
+        System.out.printf("Write Time V3 : %2.2f ms \n", nanoTimeToMillis(startTime, stopTime));
     }
 
 
