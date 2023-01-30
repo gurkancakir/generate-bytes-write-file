255e0115f37de97143683fd8af1e9649b40b6769
Gurkan Mustafa Cakir <gurkanmustafa.cakir@dxc.com>
2023-01-30 16:40:42 +0100

    writeToFile method changed

--- src/main/java/com/luxoft/FileWriter.java
+++ src/main/java/com/luxoft/FileWriter.java
@@ -3,17 +3,20 @@ package com.luxoft;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class FileWriter {
 
     public void writeToFile(byte[] contentInBytes, File file) throws IOException {
         long startTime = System.nanoTime();
-        try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
-            fileOutputStream.write(contentInBytes);
-            fileOutputStream.flush();
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw"); FileChannel fileChannel = randomAccessFile.getChannel()) {
+            ByteBuffer buff = ByteBuffer.wrap(contentInBytes);
+            fileChannel.write(buff);
         }
         long stopTime = System.nanoTime();
-        System.out.printf("Write Time V1 : %2.2f ms \n", nanoTimeToMillis(startTime, stopTime));
+        System.out.printf("Write Time V2 : %2.2f ms \n", nanoTimeToMillis(startTime, stopTime));
     }
 
 
